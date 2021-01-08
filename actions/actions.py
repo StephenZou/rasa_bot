721013dcbcfdd67e6f5430ec3fda248851321629
StephenZou <1228550726@qq.com>
2021-01-08 13:37:35 +0800

    update nlu

--- actions/actions.py
+++ actions/actions.py
@@ -13,7 +13,15 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.knowledge_base.storage import InMemoryKnowledgeBase
 from rasa_sdk.knowledge_base.actions import ActionQueryKnowledgeBase
+from rasa_sdk.forms import FormAction
+from actions.WeatherApis import get_weather_by_day
 import os
+from requests import (
+    ConnectionError,
+    HTTPError,
+    TooManyRedirects,
+    Timeout
+)
 
 
 class ActionHelloWorld(Action):
@@ -53,6 +61,85 @@ class ActionSayHello(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         username=tracker.get_slot("username")
+        print("name:{}".format(username))
         repos = "你好！{}!".format(username)
         dispatcher.utter_message(text=repos)
         return []
+
+class WeatherForm(FormAction):
+    def name(self) -> Text:
+        return "weather_form"
+    @staticmethod
+    def required_slots(tracker: Tracker) -> List[Text]:
+        """A list of required slots that the form has to fill"""
+
+        return ["date_time", "address"]
+
+    def submit(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict]:
+        """Define what the form has to do
+            after all required slots are filled"""
+        address = tracker.get_slot('address')
+        date_time = tracker.get_slot('date_time')
+
+        date_time_number = text_date_to_number_date(date_time)
+
+        if isinstance(date_time_number, str):  # parse date_time failed
+            dispatcher.utter_message("暂不支持查询 {} 的天气".format([address, date_time_number]))
+        else:
+            weather_data = get_text_weather_date(address, date_time, date_time_number)
+            print(weather_data)
+            dispatcher.utter_message(weather_data)
+        return []
+
+def get_text_weather_date(address, date_time, date_time_number):
+    try:
+        result = get_weather_by_day(address, date_time_number)
+    except (ConnectionError, HTTPError, TooManyRedirects, Timeout) as e:
+        text_message = "{}".format(e)
+    else:
+        text_message_tpl = "{} {} ({}) 的天气情况为: 白天 {}, 夜晚 {},气温:{}-{} 度"
+
+        text_message = text_message_tpl.format(
+            result['location']['name'],
+            date_time,
+            result['result']['date'],
+            result['result']['text_day'],
+            result['result']['text_night'],
+            result['result']["high"],
+            result['result']["low"],
+        )
+
+    return text_message
+
+
+def text_date_to_number_date(text_date):
+    if text_date == "今天":
+        return 0
+    if text_date == "明天":
+        return 1
+    if text_date == "后天":
+        return 2
+
+    # Not supported by weather API provider freely
+    if text_date == "大后天":
+        # return 3
+        return text_date
+
+    if text_date.startswith("星期"):
+        return text_date
+
+    if text_date.startswith("下星期"):
+        return text_date
+
+    # follow APIs are not supported by weather API provider freely
+    if text_date == "昨天":
+        return text_date
+    if text_date == "前天":
+        return text_date
+    if text_date == "大前天":
+        return text_date
